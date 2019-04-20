5dc2b8f02229000dce5ba022a52c0b93f2f29984
cpscott1 <thacam18@gmail.com>
2019-04-20 12:59:50 -0500

    added reducer

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,10 +1,9 @@
 import axios from 'axios';
 export const FETCH_START = 'FETCH_START';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
-export const FETCH_FAILURE = 'FETCH_FAILURE';
 export const ADD_START = 'ADD_START';
 export const ADD_SUCCESS = 'ADD_SUCCESS';
-export const ADD_FAILURE = 'ADD_FAILURE';
+export const FAILURE = 'ADD_FAILURE';
 
 /*
   For this project you'll need at least 2 action creators for the main portion,
@@ -25,7 +24,7 @@ export const fetchSmurfs = () => dispatch => {
     res => dispatch({ type: FETCH_SUCCESS, payload: res.data})
   )
   .catch(
-    err => dispatch({ type: FETCH_FAILURE, payload: err })
+    err => dispatch({ type: FAILURE, payload: err })
   )
 }
 
@@ -37,6 +36,6 @@ export const addSmurf = (newSmurf) => dispatch => {
     res => dispatch({ type: ADD_SUCCESS, payload: res.data })
   )
   .catch(
-    err => dispatch({ type: ADD_FAILURE, payload: err })
+    err => dispatch({ type: FAILURE, payload: err })
   )
 }
